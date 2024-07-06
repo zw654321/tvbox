98aa5283d51e6348da9aa43ed6307f814922673c
shawn82 <shawn82@gmail.com>
2024-07-07 00:53:10 +0700

    Update 爱欧乐影院.js

--- js/爱欧乐影院.js
+++ js/爱欧乐影院.js
@@ -1,16 +1,28 @@
 var rule = {
   title: '爱欧乐影院',
-  host: 'https://www.iole.app',
+  host: 'https://www.iole.app/',
   url: '/vodshow/fyclass--------fypage---.html',
-  searchUrl: '/vodsearch/page/fypage/wd/*.html',
+  searchUrl: '/vodsearch/page/fypage/wd/.html',
+  class_parse: '.top_nav li;a&&Text;a&&href;./(.?).html',
   searchable: 2,
   quickSearch: 0,
   filterable: 0,
   headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'User-Agent': 'MOBILE_UA',
   },
-  // 仅保留基本的一级列表解析
-  一级: 'body .module-item;.module-item-title&&Text;.lazyload&&data-original;.module-item-text&&Text;a&&href',
-  // 二级暂时留空
-  二级: {},
+  play_parse: true,
+  lazy: '',
+  limit: 6,
+  推荐: '.cbox_list;;;;;',
+  double: true,
+  一级: 'ul.vodlist.clearfix li;a&&title;a&&data-original;.vodname&&Text;a&&href',
+  二级: {
+    title: 'h1&&Text;.vodlist_detail_box .vodlist_title&&Text',
+    img: '.vodlist_thumb img&&data-original',
+    desc: '.vod_content .vod_content_box&&Text',
+    content: '.vod_content&&p&&Text',
+    tabs: '.play_source_tab&&a',
+    lists: '.play_source_list:eq(#id) li',
+  },
+  搜索: '*',
 }
