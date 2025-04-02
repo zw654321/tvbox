815ba9067fd0f73c1c9aef54e08bd224bcf4b55d
zw654321 <47733853+zw654321@users.noreply.github.com>
2025-04-02 14:43:00 +0800

    Update Missav.js

--- js/Missav.js
+++ js/Missav.js
@@ -1,13 +1,13 @@
 var rule = {
     title:'Missav',
-    host:'https://missav.com',
+    host:'https://missav.ai',
 	searchUrl:'/cn/search/**',
     url:'/cn/fyclass?page=fypage',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
-    class_name:'最近更新&新作上市&无码流出&SIRO&LUXU&GANA&PRESTIGE&S-CUTE&ARA&FC2&麻豆传媒&本月热门',//静态分类名称拼接
+    class_name:'最近更新&新作上市&无码流出&VR&今日热门&本周热门&本月热门&SIRO&LUXU&GANA&麻豆传媒&本月热门',//静态分类名称拼接
     class_url:'new&release&uncensored-leak&siro&luxu&gana&maan&scute&ara&fc2&madou&monthly-hot',//静态分类标识拼接
     limit:5,
     play_parse:true,
